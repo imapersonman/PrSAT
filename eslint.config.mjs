8a1ff52af7cb20bad7b9ed30238e405176bcc0da
Koissi Adjorlolo <yotcho2@gmail.com>
2025-06-11 16:34:59 -0700

    ESLint for no floating promises

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    rules: {
+      '@typescript-eslint/no-floating-promises': 'error',
+    },
+  },
+);
